aea6f8cd59f95af46de7174e51befe0f9e3f3c65
sharonbades <75455676+sharonbades@users.noreply.github.com>
2024-02-07 00:04:24 -0800

    Update User's Third Transaction [Uber SQL Interview Question].sql

--- DataLemur/MEDIUM/User's Third Transaction [Uber SQL Interview Question].sql
+++ DataLemur/MEDIUM/User's Third Transaction [Uber SQL Interview Question].sql
@@ -1,3 +1,5 @@
+/* Assume you are given the table below on Uber transactions made by users. Write a query to obtain the third transaction of every user. Output the user id, spend and transaction date.*/
+
 SELECT 
    user_id,
    spend,
